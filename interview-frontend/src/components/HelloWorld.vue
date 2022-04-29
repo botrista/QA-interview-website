3a0168a36b4f2885c226fb0162c60e4b7a551e00
Zack <class90431@gmail.com>
2022-04-29 15:45:08 +0800

    Add frontend project

---
+++ interview-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<script>
+import { ref, reactive, getCurrentInstance } from "vue";
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  created() {
+    //   $http({
+    //     url: "/api/v1/posts/list",
+    //   }).then((res) => {
+    //     let { data } = res.data;
+    //     console.log(data);
+    //   });
+  },
+};
+</script>
+
+<template>
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="address" label="地址"> </el-table-column>
+  </el-table>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ interview-frontend/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Axios from 'axios'
+
+const app = createApp(App)
+
+app.config.globalProperties.$http = Axios
+
+app.use(ElementPlus).mount('#app')
